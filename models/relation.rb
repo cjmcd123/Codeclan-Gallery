e05858b0d8f143f7db4852ac6b126c4eb4965e4f
cjmcd123 <cj_mcdowall@btinternet.com>
2018-06-10 18:05:49 +0100

    pseudo code relation.rb

---
+++ models/relation.rb
@@ -0,0 +1,87 @@
+require_relative( '../db/sql_runner' )
+
+class Biting
+
+  attr_reader( :zombie_id, :victim_id, :id )
+
+  def initialize(options)
+    # method called when creating the class instance, populates the variables
+    # from information given when creating the instance.
+  end
+
+  def save()
+    # method for saving information in to the database tables:
+    # 1. SQL code with command "INSERT", the fields being inserted & the
+    # database to insert the data into.
+    # 2. pass in the values to be inserted, seperated to allow for sanatised
+    # input.
+    # 3. run the SQL code with the values through the sql runner for sanatised
+    # input.
+    # 4. return the id created by the serial field of the database table to the
+    # instance of the class.
+
+  end
+
+  def exhibit()
+    # method for returning the exhibit:
+    # 1. SQL code with command "SELECT", the table selected from & the selection
+    # criteria.
+    # 2. pass in the value for selection.
+    # 3. run the SQL code with the values through the sql runner for sanatised
+    # input.
+    # 4. return the the data from the database and transform it into an instance
+    # of the class.
+
+  end
+
+  def category()
+    # method for returning the category:
+    # 1. SQL code with command "SELECT", the table selected from & the selection
+    # criteria.
+    # 2. pass in the value for selection.
+    # 3. run the SQL code with the values through the sql runner for sanatised
+    # input.
+    # 4. return the the data from the database and map it to an array of class
+    # instances.
+
+  end
+
+  def delete()
+    # method for deleting one relation from the database:
+    # 1. SQL code with command "DELETE" the table selected from & the selection
+    # criteria.
+    # 2. run the SQL code through the sql runner for sanatised input.
+
+  end
+
+  def self.all()
+    # method for returning all relations in the database, the "self." denotes
+    # the method is called on the class itself:
+    # 1. SQL code with command "SELECT *" & the table selected from.
+    # 2. run the SQL code through the sql runner for sanatised input.
+    # 3. return the the data from the database and map it to an array of class
+    # instances.
+
+  end
+
+  def self.find(id)
+    # method for returning one relation from the database, the "self." denotes
+    # the method is called on the class itself:
+    # 1. SQL code with command "SELECT", the table selected from & the selection
+    # criteria
+    # 2. passing in the value for selection
+    # 3. run the SQL code with the values through the sql runner for sanatised
+    # input.
+    # 4. return the the data from the database and transform it into an instance
+    # of the class.
+
+  end
+
+  def self.delete_all
+    # method for deleting all relations from the database, the "self." denotes
+    # the method is called on the class itself:
+    # 1. SQL code with command "DELETE *" & the table selected from.
+    # 2. run the SQL code through the sql runner for sanatised input.
+
+  end
+end
