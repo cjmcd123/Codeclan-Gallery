4575508027db1bc0199ecb718669feca99da9cd4
cjmcd123 <cj_mcdowall@btinternet.com>
2018-06-11 22:35:32 +0100

    check box for add category

--- controllers/category_controller.rb
+++ controllers/category_controller.rb
@@ -18,6 +18,16 @@ get '/admin/categories' do
   erb (:"categories/index")
 end
 
+get '/admin/categories/add' do
+  @categories = Category.all()
+  erb(:"categories/add")
+end
+
+get '/admin/categories/modify' do
+  @categories = Category.all()
+  erb(:"category/modify")
+end
+
 get '/admin/categories/new' do
   erb (:"categories/new")
 end
@@ -31,6 +41,12 @@ post '/admin/categories' do
   @category = Category.new(params)
   @category.save()
   erb(:"categories/create")
+end
+
+post '/admin/categories/add' do
+  @categories = params.map {|category| Category.new(category)}
+
+  erb(:"categories/check")
 end
 
 post "/admin/categories/:id" do
