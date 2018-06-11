527c1c6ccce39f69698419129ac9397af6d3435a
cjmcd123 <cj_mcdowall@btinternet.com>
2018-06-11 16:07:27 +0100

    basic exhibit views

--- controllers/exhibit_controller.rb
+++ controllers/exhibit_controller.rb
@@ -20,3 +20,45 @@ get '/exhibits/:id' do
   @exhibit = Exhibit.find(params['id'].to_i)
   erb(:"exhibits/show_visitor")
 end
+
+get '/admin/exhibits' do
+  @exhibits = Exhibit.all()
+  erb (:"exhibits/index")
+end
+
+get '/admin/exhibits/new' do
+  @artists = Artist.all()
+  @types = Type.all()
+  erb (:"exhibits/new")
+end
+
+get '/admin/exhibits/:id' do
+  @exhibit = Exhibit.find(params['id'].to_i)
+  erb(:"exhibits/show")
+end
+
+get "/admin/exhibits/:id/edit" do
+  @exhibit = Exhibit.find(params[:id].to_i)
+  @artists = Artist.all()
+  @types = Type.all()
+  @category = Category.all()
+  erb(:"exhibits/edit")
+end
+
+post '/admin/exhibits' do
+  @exhibit = Exhibit.new(params)
+  @exhibit.save()
+  erb(:"exhibits/create")
+end
+
+post "/admin/exhibits/:id" do
+  @exhibit = Exhibit.new(params)
+  @exhibit.update()
+  erb(:"exhibits/update")
+end
+
+post "/admin/exhibits/:id/delete" do
+  @exhibit = Exhibit.find(params[:id].to_i)
+  @exhibit.delete()
+  erb(:"exhibits/destroy")
+end
