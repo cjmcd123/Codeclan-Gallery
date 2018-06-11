9edf4c6185576d921b815cae5e481b0c616d58aa
cjmcd123 <cj_mcdowall@btinternet.com>
2018-06-11 10:17:08 +0100

    debug app.rb

--- controllers/category_controller.rb
+++ controllers/category_controller.rb
@@ -1,12 +1,12 @@
 require("sinatra")
 require("sinatra/contrib/all")
 require("pry-byebug")
-require_relative("./models/artist")
-require_relative("./models/category")
-require_relative("./models/exhibit")
-require_relative("./models/relation")
-require_relative("./models/type")
-also_reload("./models/*")
+require_relative("../models/artist")
+require_relative("../models/category")
+require_relative("../models/exhibit")
+require_relative("../models/relation")
+require_relative("../models/type")
+also_reload("../models/*")
 
 get '/categories' do
   @categories = Category.all()
